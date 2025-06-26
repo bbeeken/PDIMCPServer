b311ee3c385608d03dcf67b1ffe9f2bfc1618b7e
bbeeken <41312445+bbeeken@users.noreply.github.com>
2025-06-26 13:53:33 -0500

    fix: update declarative_base import

--- src/db/engine.py
+++ src/db/engine.py
@@ -6,7 +6,7 @@ from urllib.parse import quote_plus
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 logger = logging.getLogger(__name__)
 
